303492c2ed0ada424760d69eacd859ddec0cf008
Calvin Akoonyatse <cakoonyatse@gmail.com>
2024-07-24 12:01:58 +0200

    Revert "feat: add function to get urls"

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-import fetch from "node-fetch";
-const { JSDOM } = require("jsdom");
 
-function getURL() {
-	const url = process.argv[2];
-
-	if (!url) {
-		console.error("Provide a valid url as the arg");
-		process.exit(1);
-	}
-	return url;
-}
